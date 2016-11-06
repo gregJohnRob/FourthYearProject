8d54b2449badcf07138755a130d89d7a0fa30abe
Gregor Robertson <2076831r@student.gla.ac.uk>
2016-11-06 16:41:54 +0000

    A pass will now loop through each function, basic blocks and instructions and prints out every annotation

--- Code/TestCode/something.c
+++ Code/TestCode/something.c
@@ -2,10 +2,11 @@ int __attribute__((annotate("Hey \n"))) y;
 int __attribute__((annotate("Hello\n"))) z;
 
 int f(int x) {
+	int __attribute__((annotate("annotation inside of f"))) a;
 	return x;
 }
 
 int main() {
-	int x = 0;
+	int __attribute__((annotate("annotations inside of main"))) x = 0;
 	return x;
 }
